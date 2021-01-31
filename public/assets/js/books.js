a006ad968116fe977992090bee5db9b7d9b78966
Jas-F <jasminer.franklin@gmail.com>
2021-01-31 17:50:51 -0500

    add handlebar guides

---
+++ public/assets/js/books.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-book").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newsleep");
+  
+      var newSleepState = {
+        sleepy: newSleep
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/bookList/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          console.log("changed sleep to", newSleep);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBook = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/bookList", {
+        type: "POST",
+        data: newBook
+      }).then(
+        function() {
+          console.log("created new book");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    // $(".delete-cat").on("click", function(event) {
+    //   var id = $(this).data("id");
+  
+    //   // Send the DELETE request.
+    //   $.ajax("/api/bookList/" + id, {
+    //     type: "DELETE"
+    //   }).then(
+    //     function() {
+    //       console.log("deleted cat", id);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
+  
