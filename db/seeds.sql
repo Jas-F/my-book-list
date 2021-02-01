435bf4b7e37a0b5d95444139fdc0102d92928238
Jas-F <jasminer.franklin@gmail.com>
2021-01-31 20:32:01 -0500

    fix partials

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
-INSERT INTO bookList (name) values ('Rich Dad Poor Dad');
+INSERT INTO bookList (name) values ('Lords of Finance');
+INSERT INTO bookList (name) values ('Hot Commodities');
+INSERT INTO bookList (name) values ('The Sovereign Individual');
+INSERT INTO bookList (name) values ('Outliers');
+INSERT INTO bookList (name, finished) VALUES ('Rich Dad Poor Dad', true);
+INSERT INTO bookList (name, finished) VALUES ('The New Great Depression', true);
+INSERT INTO bookList (name, finished) VALUES ('How to Win at the Sport of Business', true);
+INSERT INTO bookList (name, finished) VALUES ('The Alchemist', true);
